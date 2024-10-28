a13e38cb4d59863c21611d4aafd01d1b13241b0a
2nose871 <kou1026rc@gmail.com>
2024-10-28 19:41:37 +0900

    Update gscript.js

--- gscript.js
+++ gscript.js
@@ -27,7 +27,7 @@ function setfig(num) {
 var gP = new URLSearchParams(document.location.search);
 var vd = gP.get("v");
 function iframeVideo(videoId) {
-    document.getElementById("videoPlayer").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+videoId+'?autoplay=1&loop=1&playlist='+videoId+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+    document.getElementById("videoPlayer").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+videoId+'?autoplay=1&loop=1&playlist='+videoId+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe><style>.ytp-chrome-top, .ytp-pause-overlay, .ytp-youtube-button.ytp-button{display: none;}.ytp-chrome-controls .ytp-button[aria-pressed]::after, .ytp-swatch-background-color, .ytp-settings-button.ytp-hd-quality-badge::after{background-color: white;}</style>';
 }
 
 if (vd == "true") {

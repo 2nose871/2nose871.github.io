a2d7f4bd06ce5e113900fdd95985f5e3c01cf3a9
2nose871 <kou1026rc@gmail.com>
2026-04-03 15:11:32 +0900

    Update gscript.js

--- 0c5c80777be4ffa6970024310c41bb9b5eca3613ba581491e91a91c1ee5855b6/gscript.js
+++ 0c5c80777be4ffa6970024310c41bb9b5eca3613ba581491e91a91c1ee5855b6/gscript.js
@@ -1,31 +1,45 @@
-//時計
-function setfig(num) {
-    var ret;
-    if( num < 10 ) { ret = "0" + num; }
-    else { ret = num; }
-    return ret;
- }
- 
- function set2fig(Num) {
-    var Ret;
-    if( Num < 100 ) { Ret = "0" + Num; }
-    else { Ret = Num; }
-    return Ret;
- }
- 
- function showClock1(){
- var nowTime = new Date();
- var nowHour = setfig( nowTime.getHours() );
- var nowMin = setfig( nowTime.getMinutes() );
- 
- var sTm = nowHour + ":" + nowMin;
- document.getElementById("ClockArea").innerHTML = sTm;
- }
- setInterval('showClock1()',1000);
+//接続判定
+let connection = navigator.connection;
+let isMobile = 0;
+if (connection) {
+    if (connection.type == 'cellular') {
+        isMobile = 1;
+    }
+}
+
+//軽量用設定
+let stim = document.getElementsByClassName("thumimg");
+function showToast() {
+    const toast = document.getElementById("toast");
+    toast.innerText = "モバイルデータ通信と判断したため\n軽量版を表示しています";
+    
+    // クラスを追加して表示
+    toast.classList.add("show");
+
+    // 3秒後に自動で消す
+    setTimeout(() => {
+        toast.classList.remove("show");
+    }, 5000);
+}
+// 通信環境に応じた画質を決定 (isMobileが1なら低画質、0なら高画質)
+const quality = isMobile ? "mqdefault.jpg" : "maxresdefault.jpg";
+for (let s of stim) {
+    const original = s.getAttribute("data-src");
+    s.src = original.replace("maxresdefault.jpg", quality);
+}
+if (isMobile == 0) {
+    document.getElementById("headerSub").style.display = "none";
+    document.body.style.fontFamily = "LINE Seed JP";
+}
+if (isMobile == 1) {
+    document.getElementById("headerSub").style.display = "";
+    document.body.style.fontFamily = "ヒラギノ角ゴ", "Meiryo", "sans-serif";
+    showToast();
+}
 
 //動画リンク
-var gP = new URLSearchParams(document.location.search);
-var vid = gP.get("v");
+let gP = new URLSearchParams(document.location.search);
+let vid = gP.get("v");
 function clearPlayer() {
     document.getElementById("videoPlayer").innerHTML = '';
 }
@@ -35,32 +49,52 @@ function clearPlayer() {
 function emP(vid){
     document.getElementById("videoPlayer").innerHTML = '<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="'+vid+'"></div>';
 }
+let ytid = "";
 if(vid != "" && vid != null) {
-    if(vid == "sasameshi"){emP("W0jNXAmR04c");}
-    if(vid == "ayabye"){emP("OK5QAwgd3_8");}
-    if(vid == "kinkangyoza"){emP("79isKVtXjGw");}
-    if(vid == "kochiVlog"){emP("UKWkzPtPmKA");}
-    if(vid == "midnightGame"){emP("8cqg7Vg4aZY");}
-    if(vid == "mngumi"){emP("bIcCtfqZCRw");}
-    if(vid == "mnsub"){emP("-UTecd8K0dA");}
-    if(vid == "youmeanpn"){emP("9egwNIDfNJI");}
-    if(vid == "bp3_hanami"){emP("Wal9MAt31Lw");}
-    if(vid == "bp3_manno"){emP("OE4Udt75zTc");}
-    if(vid == "killbba"){emP("8fAXvZTAySg");}
-    if(vid == "harutaParking"){emP("eaIy5zH7IZw");}
-    if(vid == "kurashikiTour"){emP("OXs_8D1XR8Y");}
-    if(vid == "kindakaiChopstick"){emP("tyuudbPxVuE");}
-    if(vid == "kawataRade"){emP("ZeNqmX3rsjM");}
-    if(vid == "mng2mk8d"){emP("QP3-W7_au7M");}
-    if(vid == "mng2mbros"){emP("jGU4GMZV-7w");}
-    if(vid == "shabon"){emP("XHxuvVIb4kk");}
-    if(vid == "parkandkeiba"){emP("1zR9GgzJC2E");}
-    if(vid == "shionoesnow"){emP("pnVaM9hX_70");}
-    if(vid == "kindafish"){emP("XIcDiFui-7c");}
-    if(vid == "gotoShinspo"){emP("XRpk_gBTvLk");}
-    if(vid == "flucall"){emP("jxToN_0IGgw");}
-    if(vid == "snowgotohell"){emP("x6vJL1xcB5g");}
+    if(vid == "sasameshi"){ytid = "W0jNXAmR04c";}
+    if(vid == "ayabye"){ytid = "OK5QAwgd3_8";}
+    if(vid == "kinkangyoza"){ytid = "79isKVtXjGw";}
+    if(vid == "kochiVlog"){ytid = "UKWkzPtPmKA";}
+    if(vid == "midnightGame"){ytid = "8cqg7Vg4aZY";}
+    if(vid == "mngumi"){ytid = "bIcCtfqZCRw";}
+    if(vid == "mnsub"){ytid = "-UTecd8K0dA";}
+    if(vid == "youmeanpn"){ytid = "9egwNIDfNJI";}
+    if(vid == "bp3_hanami"){ytid = "Wal9MAt31Lw";}
+    if(vid == "bp3_manno"){ytid = "OE4Udt75zTc";}
+    if(vid == "killbba"){ytid = "8fAXvZTAySg";}
+    if(vid == "harutaParking"){ytid = "eaIy5zH7IZw";}
+    if(vid == "kurashikiTour"){ytid = "OXs_8D1XR8Y";}
+    if(vid == "kindakaiChopstick"){ytid = "tyuudbPxVuE";}
+    if(vid == "kawataRade"){ytid = "ZeNqmX3rsjM";}
+    if(vid == "mng2mk8d"){ytid = "QP3-W7_au7M";}
+    if(vid == "mng2mbros"){ytid = "jGU4GMZV-7w";}
+    if(vid == "shabon"){ytid = "XHxuvVIb4kk";}
+    if(vid == "parkandkeiba"){ytid = "1zR9GgzJC2E";}
+    if(vid == "shionoesnow"){ytid = "pnVaM9hX_70";}
+    if(vid == "kindafish"){ytid = "XIcDiFui-7c";}
+    if(vid == "gotoShinspo"){ytid = "XRpk_gBTvLk";}
+    if(vid == "flucall"){ytid = "jxToN_0IGgw";}
+    if(vid == "snowgotohell"){ytid = "x6vJL1xcB5g";}
+}
+if (vid !== "" && vid !== null) {
+    emP(ytid);
+    document.getElementById("topmsg").style.display = "none";
 }
 console.log(vid);
 
-
+//動画情報取得
+let oEmbedUrl = `https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${ytid}&format=json`;
+if (vid != null && vid != "") {
+    async function fetchYouTubeTitle() {
+                try {
+                    const response = await fetch(oEmbedUrl);
+                    if (!response.ok) throw new Error('NW Err');
+                    const data = await response.json();
+                
+                    document.getElementById('vtitle').textContent = data.title;
+                } catch (error) {
+                    document.getElementById('vtitle').style.display = "none;"
+                }
+            }
+            fetchYouTubeTitle();
+}
